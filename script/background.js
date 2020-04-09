037d92a9aa2f5519ff9769e403cc5225aa392fd8
Flyingblu <realflyingblu@gmail.com>
2020-04-09 23:52:28 +0800

    Fix background not working bug

--- script/background.js
+++ script/background.js
@@ -17,40 +17,40 @@ chrome.runtime.onInstalled.addListener(function () {
         "title": "Directly donwload",
         "contexts": ["link"]
     });
+});
 
-    chrome.runtime.onMessage.addListener(
-        function (request, sender, sendResponse) {
-            chrome.permissions.request(
-                { origins: ['https://l.xmu.edu.my/', 'https://mymedia.xmu.edu.cn/', 'https://xmum.mediasitecloud.jp/'] },
-                function (granted) {
-                    if (granted) {
-                        if (request.name == "getLink") {
-                            chrome.storage.local.set({ 'video_id': request.video_id }, function () {
-                                chrome.windows.create({ type: 'popup', url: chrome.extension.getURL('link.html'), width: 520, height: 300 });
-                            });
-                        }
-                    }
-                });
-
-        })
-
-    chrome.contextMenus.onClicked.addListener(function (triggerInfo) {
-        if (!triggerInfo.linkUrl.includes('https://l.xmu.edu.my/mod/mediasite/view.php?id=')) {
-            chrome.windows.create({ type: 'popup', url: chrome.extension.getURL('notice.html'), width: 420, height: 230 });
-            return;
-        }
-        var video_id = /id=(\d+)/.exec(triggerInfo.linkUrl)[1];
+chrome.runtime.onMessage.addListener(
+    function (request, sender, sendResponse) {
         chrome.permissions.request(
             { origins: ['https://l.xmu.edu.my/', 'https://mymedia.xmu.edu.cn/', 'https://xmum.mediasitecloud.jp/'] },
             function (granted) {
                 if (granted) {
-                    if (triggerInfo.menuItemId === 'getDownloadLinkMenu') {
-                        chrome.storage.local.set({ 'video_id': video_id }, function () {
+                    if (request.name == "getLink") {
+                        chrome.storage.local.set({ 'video_id': request.video_id }, function () {
                             chrome.windows.create({ type: 'popup', url: chrome.extension.getURL('link.html'), width: 520, height: 300 });
                         });
-                    } else if (triggerInfo.menuItemId === 'directDownloadMenu') {
                     }
                 }
             });
-    });
+
+    })
+
+chrome.contextMenus.onClicked.addListener(function (triggerInfo) {
+    if (!triggerInfo.linkUrl.includes('https://l.xmu.edu.my/mod/mediasite/view.php?id=')) {
+        chrome.windows.create({ type: 'popup', url: chrome.extension.getURL('notice.html'), width: 420, height: 230 });
+        return;
+    }
+    var video_id = /id=(\d+)/.exec(triggerInfo.linkUrl)[1];
+    chrome.permissions.request(
+        { origins: ['https://l.xmu.edu.my/', 'https://mymedia.xmu.edu.cn/', 'https://xmum.mediasitecloud.jp/'] },
+        function (granted) {
+            if (granted) {
+                if (triggerInfo.menuItemId === 'getDownloadLinkMenu') {
+                    chrome.storage.local.set({ 'video_id': video_id }, function () {
+                        chrome.windows.create({ type: 'popup', url: chrome.extension.getURL('link.html'), width: 520, height: 300 });
+                    });
+                } else if (triggerInfo.menuItemId === 'directDownloadMenu') {
+                }
+            }
+        });
 });
